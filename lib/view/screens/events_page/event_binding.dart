b50f6564dc16bbe2a8928ea294ada51416ead1a2
Maysam-ite <maysamsamer24@gmail.com>
2023-07-28 14:18:32 +0300

    try fix controller

---
+++ lib/view/screens/events_page/event_binding.dart
@@ -0,0 +1,12 @@
+
+import 'package:get/get.dart';
+
+
+class EventBinding implements Bindings {
+  @override
+  void dependencies() {
+   // Get.lazyPut(() => EventController());
+  //  Get.lazyPut(() => AddArtistController());
+  //  Get.lazyPut(() => ArtistCardController());
+  }
+}
